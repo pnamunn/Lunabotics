d8b79b9db450ad7fba93e6a5d4c88c13edbeaa2f
pnamunn <pnamunn@gmail.com>
2023-12-11 18:43:15 -0800

    changed to launch serial_gamepad node

--- ROS2/gamepad_launch.py
+++ ROS2/gamepad_launch.py
@@ -1,14 +1,20 @@
-''' Starts a joy node & a teleop_twist_joy node'''
+''' Starts a joy node & a serial_gamepad node'''
 
-import launch
-import launch_ros.actions
+from launch import LaunchDescription
+from launch_ros.actions import Node
 import os
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    return launch.LaunchDescription([
-        launch_ros.actions.Node(package='joy', executable='joy_node', name='joy_node',
-                                arguments=['--ros-args', '--log-level', 'info']),
-        launch_ros.actions.Node(package='teleop_twist_joy', executable='teleop_node', name='teleop_twist_joy_node',
-                                arguments=['--ros-args', '--log-level', 'info'])
+    return LaunchDescription([
+        Node(package='joy', 
+             executable='joy_node', 
+             namespace='gamepad_control',       # groups the node into this system
+             name='joy_node',                   # names the node
+        ),
+        Node(package='gamepad_pkg', 
+             executable='serial_gamepad',
+             namespace='gamepad_control',   
+             name='serial_gamepad_node',
+        )
     ])
