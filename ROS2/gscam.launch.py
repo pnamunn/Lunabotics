ade61a349b5b27ee8464c9a9a6f95513b53952d7
pnamunn <pnamunn@gmail.com>
2023-12-15 21:31:38 -0800

    fixed boolean errors

--- ROS2/gscam.launch.py
+++ ROS2/gscam.launch.py
@@ -8,13 +8,14 @@ def generate_launch_description():
     Node(package='gscam',
          executable='gscam_node',
          name='gscam_node',
+         output='screen'
          parameters=[
             {'camera_info_url' : 'package://gscam/examples/uncalibrated_parameters.ini'},
-            {'gscam_config' : 'v4l2src do-timestamp=true ! video/x-raw ! jpegenc ! multipartmux ! multipartdemux ! jpegparse'},
+            {'gscam_config' : 'v4l2src do-timestamp=true ! video/x-raw,framerate=30/1 ! jpegenc ! multipartmux ! multipartdemux ! jpegparse'},
             {'camera_name' : 'default'},
             {'image_encoding' : 'jpeg'},
-            {'use_gst_timestamps' : 'true'},
-            {'sync_sink' : 'true'},
+            {'use_gst_timestamps' : True},
+            {'sync_sink' : True}
          ]
     )
   ])
