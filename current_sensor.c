c19e7bdd6702b21a3744ef9f8688d13daafeb3dc
anabelrsanchez <146898998+anabelrsanchez@users.noreply.github.com>
2024-03-04 16:40:02 -0800

    Update current_sensor.c

--- current_sensor.c
+++ current_sensor.c
@@ -13,12 +13,8 @@
 //#define BAUD 115200					ignore
 //#define BAUDRATE ((F_CPU)/(BAUD*16UL))-1		ignore
 #define Vcc 5.0
-volatile uint16_t voltage = 0;
-volatile uint16_t current = 0;
-//unsigned char hello[] = "hello";
-unsigned char newline[] = "\n";
-unsigned char carriage[] = "\r";
-unsigned char mA[] = " mA";
+float voltage = 0.0;
+float current = 0.0;
 
 void ADC_init() {
 	ADCSRA |= (1<<ADEN);	// enable ADC
@@ -26,52 +22,117 @@ void ADC_init() {
 	ADCSRA |= (1<<ADIE);	// enable ADC conversion complete interrupt
 	ADCSRA |= (1<<ADPS0) | (1<<ADPS1) | (1<<ADPS2); // prescale 128 
 	ADMUX |= (1<<REFS0);	// AVcc is our reference voltage
-	ADMUX |= (1<<MUX1);	// ADC2, PC2
+	ADMUX |= (1<<MUX1);		// ADC2, PC2
 	ADCSRA |= (1<<ADSC);	// start first conversion
 }
 
-void serial_transmit(unsigned char data[]) {
-	int i = 0;
-	while (data[i] != '\0') {
-		while (!(UCSR0A & (1<<UDRE0)));
-		UDR0 = data[i];
-		i++;
-	}
-
+void UART_init() {
+	UBRR0L = 8;   				// for baud rate of 115200
+	UCSR0B |= (1<<TXEN0);		// enable transmitter, 8 bit size is default
+	UCSR0B |= (1<<RXEN0);		// enable receiver
 }
 
+void serial_transmit(uint8_t data) {
+	while (!(UCSR0A & (1<<UDRE0)));
+	UDR0 = data;
+}
+//void other_serial_transmit(unsigned char data[]) {
+	//int i = 0;
+    //while (data[i] != '\0') {
+	    //while (!(UCSR0A & (1<<UDRE0)));   				// used for debugging, ignore
+		//UDR0 = data[i];
+		//i++; }
+//}
+
+// format is XXXX.XXX 
+void printFloat(float num) {
+	uint16_t integer_part = num;
+	uint16_t decimal_part = (num - integer_part)*1000;
+	uint8_t first_digit = integer_part / 1000;
+	if (first_digit < 0 || first_digit > 9) {
+		serial_transmit('0');
+	}
+	else {
+		serial_transmit(first_digit + '0');
+	}
+	uint8_t second_digit = (integer_part - (first_digit * 1000)) / 100;
+	if (second_digit < 0 || second_digit > 9) {
+		serial_transmit('0');
+	}
+	else {
+		serial_transmit(second_digit + '0');
+	}
+	uint8_t third_digit = (integer_part - (first_digit*1000 + second_digit*100)) / 10;
+	if (third_digit < 0 || third_digit > 9) {
+		serial_transmit('0');
+	}
+	else {
+		serial_transmit(third_digit + '0');
+	}
+	uint8_t fourth_digit = (integer_part - (first_digit*1000 + second_digit*100 + third_digit*10));
+	if (fourth_digit < 0 || fourth_digit > 9) {
+		serial_transmit('0');
+	}
+	else {
+		serial_transmit(fourth_digit + '0');
+	}
+	serial_transmit('.');
+	uint8_t tenths = decimal_part / 100;
+	if (tenths < 0 || tenths > 9) {
+		serial_transmit('0');
+	}
+	else {
+		serial_transmit(tenths + '0');
+	}
+	uint8_t hundreths = (decimal_part - tenths*100) / 10;
+	if (hundreths < 0 || hundreths > 9) {
+		serial_transmit('0');
+	}
+	else {
+		serial_transmit(hundreths + '0');
+	}
+	uint8_t thousandths = (decimal_part - tenths*100 - hundreths*10);
+	if (thousandths < 0 || thousandths > 9) {
+		serial_transmit('0');
+	}
+	else {
+		serial_transmit(thousandths + '0');
+	}
+}
 
 ISR (ADC_vect) {	
-static uint8_t ADC_low = ADCL;
-static uint8_t ADC_high = ADCH;
-static uint16_t ADC_full = 0;
-ADC_full = (ADC_high << 8) | ADC_low;
-
-voltage = (Vcc*(ADC_full/1024.0) - Vcc/1.99)*100000;	// multiply by big number to get reading
-current = (voltage / 4.0);				// supposed to be 0.04 but voltage is already big
-static unsigned char msg[] = "";; 
-itoa(ADC_full, msg, 10);	// convert ADC_full to char[], decimal
-serial_transmit(msg);		// transmit ADC_full
-serial_transmit(newline);
-_delay_ms(500);
+	static uint16_t ADC_low = 0;
+	static uint16_t ADC_high = 0;
+	static uint16_t ADC_full = 0;
+	
+	ADC_low = ADCL;
+	ADC_high = ADCH;
+	ADC_full = (ADC_high << 8) | ADC_low;
+	
+	voltage = (Vcc / 1024.0)*ADC_full;	
+	voltage = voltage - (Vcc / 2.0);
+	current = voltage*25000.0;
+	printFloat(current);
+	serial_transmit(' ');
+	serial_transmit('m');
+	serial_transmit('A');
+	_delay_ms(500);
+	serial_transmit('\n');
 }
-
 
 int main(void)
 {
-    UBRR0L = 8;   			// for baud rate of 115200		
-	UCSR0B |= (1<<TXEN0);		// enable transmitter, 8 bit size is default
-	UCSR0B |= (1<<RXEN0);		// enable receiver
-	DDRC &= ~(1<<2);		// Input PC2, ADC2 (for uno)
-	//DDRF &= ~(1<<0);		// Input PF0, ADC0 (for mega)
-    sei();				// enable interrupts
+	sei();				// enable interrupts
+	
+	UART_init();
 	ADC_init();
+	DDRC &= ~(1<<2);	// Input PC2, ADC2 (for uno)
+	
+	ADCSRA |= (1<<ADSC);	// start first conversion
     while (1) 
-    {
-
+    {	
     }
 }
-
 
 
 
