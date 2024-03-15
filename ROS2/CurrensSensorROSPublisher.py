88250dfbba9b88ccca0f34efa54249308f4899db
Nick-Swany <145230382+Nick-Swany@users.noreply.github.com>
2024-03-14 17:24:55 -0700

    Update CurrensSensorROSPublisher.py
    Fixed the callback. Code now comiles

--- ROS2/CurrensSensorROSPublisher.py
+++ ROS2/CurrensSensorROSPublisher.py
@@ -11,24 +11,30 @@ from std_msgs.msg import String
 
 class CurrentSensorPublisher(Node):
     def __init__(self):
-        super().__init__('current service')
+        super().__init__('CurrentPublisher')
         #create a publisher node of type= KeyValue, topic= current, callback= current_callback, 
-        self.publisher_ = self.create_publisher(String, 'current', self.current_callback, 10)
-        #timer_period = 0.5  # seconds
-        #self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.publisher_ = self.create_publisher(String, 'current', 10)
+        
+        self.get_logger().info("Pubber node has been created")
+        
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
 
 
-    def current_callback(self, request, response): #only publish if a problem arrises
-        ser = serial.Serial('/dev/ttyACM0', 9600, timeout = 1) #TODO find out of this is correct port
-        request.key = 'LeftBack'                               #name of wheel with problem
-        request.value = 0                           
-
-        if ser < 1: #if less than an amp of current alert 
-            request.value = ser
-            msg = ('Problem with wheel "%s", currrent is "%s".' %request.key %request.value)
+    def timer_callback(self): #only publish if a problem arrises
+        #TODO fix Serial
+        #ser = serial.Serial('/dev/ttyACM0', 9600, timeout = 1) #TODO find out of this is correct port
+        msg = String()
+        msg.data = 'LeftBack'                               #name of wheel with problem
+        #request.value = 0                           
+        volts = 1
+        if volts > 1: #if less than an amp of current alert 
+            msg = ('Problem with wheel "%s", current sesnor is reading "%s"volts.' %msg.data %volts)
             self.publisher_.publish(msg)
             self.get_logger().info(msg)
-        time.wait(1)
+
+
+        #time.wait(1)
 
 def main(args=None):
     rclpy.init(args=args)           #get rclpy library
