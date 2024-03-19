4d1cdc280650829617307461bb42bca2ac1823ec
pnamunn <pnamunn@gmail.com>
2024-03-18 19:56:45 -0700

    a

--- ROS2/CurrensSensorROSSubber.py
+++ ROS2/CurrensSensorROSSubber.py
@@ -1,46 +1,36 @@
-import serial
 import rclpy
 from rclpy.node import Node
 import time
 from std_msgs.msg import String
+
 #from diagnostic_msgs import KeyValue.msg
+#ommon_interfaces/diagnostic_msgs
 
 #Service node will recieve an error message from the client
 #will print out thier is a drop in current 
 #will wait three seconds and then send an aknoledgement
 
-class CurrentSensorPublisher(Node):
+class CurrentSensorSubscriber(Node):
     def __init__(self):
-        super().__init__('CurrentSubber')
+        super().__init__('CurrentSubscriber')
         #create a publisher node of type= KeyValue, topic= current, callback= current_callback, 
-        self.subber = self.create_subscription(String, 'current', self.current_callback, 10)
-        #timer_period = 0.5  # seconds
-        self.get_logger().info("Subber node has been created")
-
-        #self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.subscriber_ = self.create_subscription(String, 'current',self.current_callback, 10)
+        self.get_logger().info("Pubber node has been created")
 
 
-    def current_callback(self, request, response): #only publish if a problem arrises
-        ser = serial.Serial('/dev/ttyACM0', 9600, timeout = 1) #TODO find out of this is correct port
-        request.key = 'LeftBack'                               #name of wheel with problem
-        request.value = 0                           
+    def current_callback(self, msg): #only publish if a problem arrises  
+        self.get_logger().info(msg)
 
-        if ser < 1: #if less than an amp of current alert 
-            request.value = ser
-            msg = ('Problem with wheel "%s", currrent is "%s".' %request.key %request.value)
-            self.publisher_.publish(msg)
-            self.get_logger().info(msg)
-        print('success2')
-        time.wait(1)
+
 
 def main(args=None):
     rclpy.init(args=args)           #get rclpy library
-    pubber = CurrentSensorPublisher()      #create a node
+    pubber = CurrentSensorSubscriber()      #create a node
 
 
     rclpy.spin(pubber)            #run until it is killed
 
-    print('Success1')
+
     pubber.destroy_node()         #free node
     rclpy.shutdown()               #stop RoS
 
